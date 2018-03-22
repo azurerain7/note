36a10fab4b7e5be8e3d81c2c65f06dc8ed86eee9
zhao heng <azurerain7@users.noreply.github.com>
2018-03-22 11:43:51 +0800

    Add files via upload

---
+++ caffe_install_readme.sh
@@ -0,0 +1,14 @@
+apt-get update
+apt-get install -y wget
+apt-get install -y vim
+apt-get install -y git
+apt-get install -y libprotobuf-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+apt-get install -y --no-install-recommends libboost-all-dev
+apt-get install -y libatlas*
+
+
+
+
+
+
+
