4152828ec6888cd0b171850c61dc47b92d3d86ef
zhao heng <azurerain7@users.noreply.github.com>
2018-03-22 18:07:50 +0800

    Update caffe_install_readme.sh

--- caffe_install_readme.sh
+++ caffe_install_readme.sh
@@ -5,8 +5,9 @@ apt-get install -y git
 apt-get install -y libprotobuf-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
 apt-get install -y --no-install-recommends libboost-all-dev
 apt-get install -y libatlas*
+apt-get install libopenblas-dev
 
-
+#sudo ln -s /usr/lib/x86_64-linux-gnu/libturbojpeg.so.0.0.0 /usr/lib/x86_64-linux-gnu/libturbojpeg.so
 
 
 
